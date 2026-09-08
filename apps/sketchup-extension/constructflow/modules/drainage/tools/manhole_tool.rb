1bc3b94c463bc6057a054bb23800ce3bdbc85fb3
Jirayu WAngloklang <dullahan34@hotmail.com>
2026-09-08 15:27:24 +0000

    feat(drainage): add interactive manhole placement tool

---
+++ apps/sketchup-extension/constructflow/modules/drainage/tools/manhole_tool.rb
@@ -0,0 +1,108 @@
+# frozen_string_literal: true
+
+module JiraNot
+  module ConstructFlow
+    module Drainage
+      module Tools
+        class ManholeTool
+          def initialize(runtime:, size_mm:, cover_level_mm:, invert_in_mm:, invert_out_mm:)
+            @runtime = runtime
+            @size_mm = size_mm
+            @cover_level_mm = cover_level_mm
+            @invert_in_mm = invert_in_mm
+            @invert_out_mm = invert_out_mm
+            @input_point = Sketchup::InputPoint.new
+          end
+
+          def activate
+            Sketchup.set_status_text('ConstructFlow Drainage: click to place manhole. Esc to finish.', SB_PROMPT)
+          end
+
+          def onMouseMove(_flags, x, y, view)
+            @input_point.pick(view, x, y)
+            view.invalidate
+          end
+
+          def draw(view)
+            @input_point.draw(view) if @input_point.valid?
+          end
+
+          def onLButtonDown(_flags, x, y, view)
+            @input_point.pick(view, x, y)
+            return unless @input_point.valid?
+
+            result = @runtime.commands.execute(
+              'PlaceManhole',
+              {
+                location_mm: Core::Units.point_to_mm(@input_point.position),
+                size_mm: @size_mm,
+                cover_level_mm: @cover_level_mm,
+                invert_in_mm: @invert_in_mm,
+                invert_out_mm: @invert_out_mm
+              },
+              project_id: @runtime.project.project_id
+            )
+            if result[:status] == 'success'
+              view.invalidate
+              Sketchup.set_status_text('Manhole placed. Click again or Esc to finish.', SB_PROMPT)
+            else
+              UI.messagebox(result[:errors].join("\n"))
+            end
+          rescue StandardError => error
+            UI.messagebox("ConstructFlow Drainage error: #{error.message}")
+          end
+
+          def onCancel(_reason, _view)
+            @runtime.active_model.select_tool(nil)
+          end
+        end
+
+        class RelocateManholeTool
+          def initialize(runtime:, manhole_object_id:)
+            @runtime = runtime
+            @manhole_object_id = manhole_object_id.to_s
+            @input_point = Sketchup::InputPoint.new
+          end
+
+          def activate
+            Sketchup.set_status_text('ConstructFlow Drainage: click new manhole location. Esc to cancel.', SB_PROMPT)
+          end
+
+          def onMouseMove(_flags, x, y, view)
+            @input_point.pick(view, x, y)
+            view.invalidate
+          end
+
+          def draw(view)
+            @input_point.draw(view) if @input_point.valid?
+          end
+
+          def onLButtonDown(_flags, x, y, view)
+            @input_point.pick(view, x, y)
+            return unless @input_point.valid?
+
+            result = @runtime.commands.execute(
+              'RelocateManhole',
+              {
+                object_id: @manhole_object_id,
+                new_location_mm: Core::Units.point_to_mm(@input_point.position)
+              },
+              project_id: @runtime.project.project_id
+            )
+            if result[:status] == 'success'
+              @runtime.active_model.select_tool(nil)
+            else
+              UI.messagebox(result[:errors].join("\n"))
+            end
+          rescue StandardError => error
+            UI.messagebox("ConstructFlow Relocate Manhole error: #{error.message}")
+          end
+
+          def onCancel(_reason, _view)
+            @runtime.active_model.select_tool(nil)
+          end
+        end
+      end
+    end
+  end
+end
