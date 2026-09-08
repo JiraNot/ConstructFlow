aaa4b89731d05d789b24701e9a7bc4d8520dcdb9
Jirayu WAngloklang <dullahan34@hotmail.com>
2026-09-08 11:08:27 +0000

    feat(extension): add SketchUp extension loader shell

---
+++ apps/sketchup-extension/constructflow.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'sketchup.rb'
+require 'extensions.rb'
+
+module JiraNot
+  module ConstructFlow
+    EXTENSION_ID = 'constructflow'
+    EXTENSION_NAME = 'ConstructFlow'
+    VERSION = '0.1.0-alpha.1'
+
+    unless file_loaded?(__FILE__)
+      extension = SketchupExtension.new(EXTENSION_NAME, 'constructflow/main')
+      extension.description = 'Modular design-to-construction platform for SketchUp.'
+      extension.version = VERSION
+      extension.creator = 'JiraNot'
+      extension.copyright = 'Copyright JiraNot'
+
+      Sketchup.register_extension(extension, true)
+      file_loaded(__FILE__)
+    end
+  end
+end
