73c0d347dde2bc2c069dd0aebf8d7c84added987
Jirayu WAngloklang <dullahan34@hotmail.com>
2026-09-10 04:14:24 +0000

    feat(roof): load extension regeneration adapter

--- apps/sketchup-extension/constructflow/bootstrap.rb
+++ apps/sketchup-extension/constructflow/bootstrap.rb
@@ -5,6 +5,7 @@ require_relative 'core/entity_guard'
 require_relative 'core/geometry_guard'
 require_relative 'modules/extension/execution_runner'
 require_relative 'modules/structure/extension_command_registration'
+require_relative 'modules/roof/extension_command_registration'
 require_relative 'modules/extension/runtime_integration'
 
 JiraNot::ConstructFlow::Extension::RuntimeIntegration.install(JiraNot::ConstructFlow::Runtime)
