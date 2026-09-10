810d6c9131262492bbab7442eb9953d65de76c98
Jirayu WAngloklang <dullahan34@hotmail.com>
2026-09-10 04:35:22 +0000

    feat(runtime): install drainage plan representation providers

--- apps/sketchup-extension/constructflow/bootstrap.rb
+++ apps/sketchup-extension/constructflow/bootstrap.rb
@@ -8,6 +8,9 @@ require_relative 'core/representation_runtime_integration'
 require_relative 'modules/extension/execution_runner'
 require_relative 'modules/structure/extension_command_registration'
 require_relative 'modules/extension/runtime_integration'
+require_relative 'modules/drainage/plan_representation_provider'
+require_relative 'modules/drainage/representation_registration'
 
 JiraNot::ConstructFlow::Core::RepresentationRuntimeIntegration.install(JiraNot::ConstructFlow::Runtime)
+JiraNot::ConstructFlow::Drainage::RepresentationRegistration.install(JiraNot::ConstructFlow::Runtime)
 JiraNot::ConstructFlow::Extension::RuntimeIntegration.install(JiraNot::ConstructFlow::Runtime)
