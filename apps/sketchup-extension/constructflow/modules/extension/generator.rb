c7c6c53f18980b0c560ebbc3c301e87f3ef08305
Jirayu WAngloklang <dullahan34@hotmail.com>
2026-09-10 15:44:51 +0000

    feat(extension): add opt-in attachment opening intent

--- apps/sketchup-extension/constructflow/modules/extension/generator.rb
+++ apps/sketchup-extension/constructflow/modules/extension/generator.rb
@@ -6,6 +6,7 @@ module JiraNot
       class Generator
         DEFAULTS = {
           'architecture' => { 'enabled' => true, 'walls' => 'auto' },
+          'opening' => { 'enabled' => false },
           'structure' => { 'enabled' => true, 'foundation' => 'auto' },
           'surface' => { 'enabled' => true, 'floor' => 'auto' },
           'roof' => { 'enabled' => true, 'system' => 'from_extension' },
