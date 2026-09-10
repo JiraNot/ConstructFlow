fe9aab6f920baefcc40a5f25cea6a22adf4949c7
Jirayu WAngloklang <dullahan34@hotmail.com>
2026-09-10 03:07:09 +0000

    feat(extension): add deterministic domain intent generator

---
+++ apps/sketchup-extension/constructflow/modules/extension/generator.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+module JiraNot
+  module ConstructFlow
+    module Extension
+      class Generator
+        DEFAULTS = {
+          'structure' => { 'enabled' => true, 'foundation' => 'auto' },
+          'surface' => { 'enabled' => true, 'floor' => 'auto' },
+          'roof' => { 'enabled' => true, 'system' => 'from_extension' },
+          'drainage' => { 'enabled' => true, 'rainwater' => true, 'surface_drainage' => true },
+          'electrical' => { 'enabled' => true },
+          'interior' => { 'enabled' => true }
+        }.freeze
+
+        def initialize(definition)
+          raise ArgumentError, 'extension definition is required' unless definition
+
+          @definition = definition
+        end
+
+        def intents(options = {})
+          options = stringify_keys(options)
+          overrides = stringify_keys(options.fetch('domains', {}))
+          domains = deep_merge(DEFAULTS, overrides)
+          {
+            'extension_id' => options['extension_id'],
+            'program' => @definition.program,
+            'mode' => @definition.mode,
+            'boundary_mm' => @definition.boundary_mm,
+            'base_level_id' => @definition.base_level_id,
+            'base_offset_mm' => @definition.base_offset_mm,
+            'target_height_mm' => @definition.target_height_mm,
+            'roof_intent' => @definition.roof_intent,
+            'attachment_host_id' => @definition.attachment_host_id,
+            'domains' => domains
+          }.freeze
+        end
+
+        def enabled_domains(options = {})
+          intents(options).fetch('domains').each_with_object([]) do |(domain, config), result|
+            result << domain if config.is_a?(Hash) && config['enabled']
+          end.freeze
+        end
+
+        private
+
+        def stringify_keys(value)
+          return value unless value.is_a?(Hash)
+
+          value.each_with_object({}) do |(key, item), result|
+            result[key.to_s] = stringify_keys(item)
+          end
+        end
+
+        def deep_merge(base, override)
+          base.each_with_object({}) do |(key, value), result|
+            replacement = override[key]
+            result[key] = if value.is_a?(Hash) && replacement.is_a?(Hash)
+                            deep_merge(value, replacement)
+                          elsif override.key?(key)
+                            replacement
+                          else
+                            value
+                          end
+          end
+        end
+      end
+    end
+  end
+end
