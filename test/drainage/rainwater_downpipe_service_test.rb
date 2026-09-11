9f98a98324cc4fb96c0ae0205f8905dddb4e4729
Jirayu WAngloklang <dullahan34@hotmail.com>
2026-09-11 08:08:35 +0000

    test(drainage): prove downpipe endpoint regeneration

--- test/drainage/rainwater_downpipe_service_test.rb
+++ test/drainage/rainwater_downpipe_service_test.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative '../test_helper'
+require File.join(DRAINAGE, 'downpipe_definition')
 require File.join(DRAINAGE, 'rainwater_downpipe_service')
 
 class RainwaterDownpipeServiceTest < Minitest::Test
@@ -11,15 +12,21 @@ class RainwaterDownpipeServiceTest < Minitest::Test
   end
 
   class FakeGeometry
-    attr_reader :definitions
+    attr_reader :definitions, :rebuilt
 
     def initialize
       @definitions = []
+      @rebuilt = []
     end
 
     def create_pipe_group(_model, definition)
       @definitions << definition
       Group.new
+    end
+
+    def rebuild_pipe!(entity, definition)
+      @rebuilt << [entity, definition]
+      entity
     end
   end
 
@@ -106,5 +113,48 @@ class RainwaterDownpipeServiceTest < Minitest::Test
       service.create(start_connector_id: @source['id'], end_connector_id: @target['id'])
     end
     assert_includes error.message, 'already has an active rainwater connection'
+  end
+
+  def test_refresh_preserves_downpipe_and_connection_identity_for_direct_route
+    repository = JiraNot::ConstructFlow::Drainage::Repository.new
+    service = JiraNot::ConstructFlow::Drainage::RainwaterDownpipeService.new(
+      runtime: @runtime, repository: repository, geometry: @geometry
+    )
+    created = service.create(start_connector_id: @source['id'], end_connector_id: @target['id'])
+    object = created[:object]
+    connection_id = created[:connection]['id']
+
+    @connectors.update_connector(@source['id'], position_mm: [500, 0, 3200])
+    refreshed = service.refresh_for_start_connector(start_connector_id: @source['id'])
+    definition = repository.read_downpipe(object.entity)
+
+    assert_equal [object.id], refreshed[:updated_object_ids]
+    assert_equal connection_id, definition.connection_id
+    assert_equal [[500.0, 0.0, 3200.0], [500.0, 0.0, 0.0], [1500.0, 0.0, 0.0]], definition.route_nodes_mm
+    assert_equal 1, @geometry.rebuilt.length
+    assert_equal object.entity, @geometry.rebuilt.first[0]
+  end
+
+  def test_refresh_reanchors_custom_route_endpoints_but_keeps_interior_nodes
+    repository = JiraNot::ConstructFlow::Drainage::Repository.new
+    service = JiraNot::ConstructFlow::Drainage::RainwaterDownpipeService.new(
+      runtime: @runtime, repository: repository, geometry: @geometry
+    )
+    created = service.create(
+      start_connector_id: @source['id'],
+      end_connector_id: @target['id'],
+      route_nodes_mm: [[0, 0, 3000], [700, 250, 1400], [1500, 0, 0]],
+      route_strategy: 'manual'
+    )
+
+    @connectors.update_connector(@source['id'], position_mm: [100, 50, 3200])
+    @connectors.update_connector(@target['id'], position_mm: [1600, 200, 100])
+    service.refresh_for_start_connector(start_connector_id: @source['id'])
+    definition = repository.read_downpipe(created[:object].entity)
+
+    assert_equal [100.0, 50.0, 3200.0], definition.route_nodes_mm.first
+    assert_equal [700.0, 250.0, 1400.0], definition.route_nodes_mm[1]
+    assert_equal [1600.0, 200.0, 100.0], definition.route_nodes_mm.last
+    assert_equal created[:connection]['id'], definition.connection_id
   end
 end
